6d9510250f1cfc4575913c8ef413a5369701e1e6
FelipeECarvalho <Felipeemanuelc12@gmail.com>
2021-11-07 20:40:40 -0300

    Começando a implementar a visualização do tabuleiro

---
+++ Xadrez-console/Tela.cs
@@ -0,0 +1,26 @@
+﻿using tabuleiro;
+using System;
+namespace Xadrez_console
+{
+	class Tela
+	{
+		public static void ImprimeTabuleiro(Tabuleiro tabuleiro) 
+		{
+			for (int i = 0; i < tabuleiro.Linhas; i++) 
+			{
+				for (int j = 0; j < tabuleiro.Colunas; j++) 
+				{
+					if (tabuleiro.Peca(i, j) != null)
+					{
+						Console.Write($" {tabuleiro.Peca(i, j)} ");
+					}
+					else 
+					{
+						Console.Write(" - ");
+					}
+				}
+				System.Console.WriteLine();
+			}
+		}
+	}
+}
