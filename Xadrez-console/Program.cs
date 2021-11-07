38539323f853e3c3fa2d54d39f6b0c132d2e197d
FelipeECarvalho <Felipeemanuelc12@gmail.com>
2021-11-07 18:20:12 -0300

    Criação da classe posição

---
+++ Xadrez-console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xadrez_console.Tabuleiro;
+
+namespace Xadrez_console
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Posicao tab = new Posicao(3, 4);
+			Console.WriteLine(tab);
+		}
+	}
+}
